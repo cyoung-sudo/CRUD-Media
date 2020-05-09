b20eb53edff792f12ce702c5c0b757d0155ff88e
“cyoung-sudo” <cyoungcs20@gmail.com>
2020-05-08 20:27:34 -0700

    added follow delete association

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,5 @@ class User < ApplicationRecord
   has_many :received_follows, foreign_key: :followee_id, class_name: "Follow"
   has_many :followers, through: :received_follows, source: :follower
   has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
-  has_many :followings, through: :given_follows, source: :followee
+  has_many :followings, through: :given_follows, source: :followee, dependent: :destroy
 end
