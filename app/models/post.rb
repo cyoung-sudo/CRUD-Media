d0435fe428a42962667c213a49b4c875b28820e3
“cyoung-sudo” <cyoungcs20@gmail.com>
2020-05-11 17:12:37 -0700

    updated validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 2, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 2, maximum: 300 }
+  validates :title, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 500 }
   validates :user_id, presence: true
 
   belongs_to :user
